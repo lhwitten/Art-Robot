0046ef7124176f96b3de692dbeab515db780ef51
Olga Pidruchna <opidruchna@olin.edu>
2022-12-12 17:32:49 -0500

    save changes

--- serial_communication/serial_communication.py
+++ serial_communication/serial_communication.py
@@ -1,12 +1,12 @@
 import serial
 import time
 
-communication_port = '/dev/tty.usbmodem143301'
-arduino = serial.Serial(port=communication_port, baudrate=115200, timeout=.1)
+communication_port = 'COM8'
+arduino = serial.Serial(port=communication_port, baudrate=57600, timeout=1)
 
-gcode_file = 'straight_line_test.gcode.txt'
+# gcode_file = "C:\Users\opidruchna\OneDrive - Olin College of Engineering\Documents\PIE\Art-Robot\serial_communication"
 
-file = open(gcode_file,'r')
+file = open(r"C:\\Users\\opidruchna\\OneDrive - Olin College of Engineering\Documents\\PIE\Art-Robot\serial_communication",'r')
 lines = file.readlines()
 
 def write_read(x):
