f9b4356d1c13c1089df7613816b9e65cc52d8051
Olga Pidruchna <olgapidruchna@gmail.com>
2022-12-13 13:10:52 -0500

    Checks whether arduino has finished cmd before sending new msg

--- serial_communication/serial_communication.py
+++ serial_communication/serial_communication.py
@@ -1,28 +1,23 @@
 import serial
 import time
 
-communication_port = 'COM5'
+communication_port = 'COM8'
 arduino = serial.Serial(port=communication_port, baudrate=57600, timeout=1)
 
 gcode_file = 'star.gcode'
 
-file = open(r"C:\\Users\\opidruchna\\OneDrive - Olin College of Engineering\Documents\\PIE\Art-Robot\serial_communication", 'r')
+file = open(gcode_file, 'r')
 lines = file.readlines()
 
 
-def write_read(x, index):
-    arduino.write(x.encode())
-    if index != 1 and index != 2:
-        time.sleep(4)
-    else:
-        time.sleep(4)
-    data = 0
-    #data = arduino.readline()
-    return data
+def write_read(x):
+    data = arduino.readline()
+    if data == "> ":
+        arduino.write(x.encode())
+    print(x.encode)
 
 
-index = 0
+# index = 0
 for line in lines:
-    index += 1
-    data = write_read(line, index)
-    print(data)
+    write_read(line)
+    # index += 1
